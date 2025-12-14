478ab427334f9d9a4520864c672cf5e28eb27ef0
Joan Maina <mainajoan555@gmail.com>
2025-12-14 18:21:26 +0300

    app configuration

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
+       <Header className='header'/>
       <Routes>
-        <Route path="/" element={<h1>Hello, World!</h1>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
+
+      <Footer className='footer'/>
     </Router>
   )
 }
