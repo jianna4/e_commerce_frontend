5e6ee7c34cd265cf8405e4ade641fcacfbd8beb8
Joan Maina <mainajoan555@gmail.com>
2025-12-16 19:58:56 +0300

    hero image

--- app/src/components/category components/hero.jsx
+++ app/src/components/category components/hero.jsx
@@ -1,4 +1,13 @@
+const BASE_URL = "http://127.0.0.1:8000";
+
 const CategoryHero = ({ category }) => {
+  // Handle image URL
+  const imageUrl = category.image
+    ? category.image.startsWith("http")
+      ? category.image
+      : `${BASE_URL}${category.image}`
+    : "/placeholder.png"; // fallback if no image
+
   return (
     <div className="bg-gray-100 min-h-screen pt-6">
       <div className="max-w-7xl mx-auto px-6 py-16">
@@ -6,8 +15,9 @@ const CategoryHero = ({ category }) => {
           
           {/* LEFT: Text */}
           <div>
-            <h1 className="text-5xl font-bold text-gray-900">Explore our
-             <div className="text-green-950"> {category.name}</div>
+            <h1 className="text-5xl font-bold text-gray-900">
+              Explore our
+              <div className="text-green-950"> {category.name}</div>
             </h1>
 
             <p className="mt-6 text-lg text-gray-600 max-w-xl">
@@ -18,7 +28,7 @@ const CategoryHero = ({ category }) => {
           {/* RIGHT: Image */}
           <div className="flex justify-center md:justify-end">
             <img
-              src={category.image}
+              src={imageUrl}
               alt={category.name}
               className="
                 w-full
