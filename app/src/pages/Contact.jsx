7b0bb9ca471110f94fd4a97149d91437256288ae
Joan Maina <mainajoan555@gmail.com>
2025-12-16 23:28:47 +0300

    ...name

--- app/src/pages/Contact.jsx
+++ app/src/pages/Contact.jsx
@@ -4,10 +4,10 @@ import img from "../assets/logo.png"; // Replace with your image
 
 function Contact() {
   return (
-    <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen shadow-2xl flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl w-full flex flex-col lg:flex-row items-stretch gap-8">
         {/* Left: Form */}
-        <div className="flex-1 bg-gray-100 p-8 rounded-lg shadow-lg ">
+        <div className="flex-1  p-8 rounded-lg shadow-lg ">
           <h2 className="text-4xl font-bold mb-4">Contact Sales</h2>
           <p className="mb-6 text-black">
             Fill out the form and our team will get back to you shortly.
@@ -18,33 +18,33 @@ function Contact() {
               <input
                 type="text"
                 placeholder="First Name"
-                className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+                className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-200  focus:outline-none focus:ring-2 focus:ring-green-950"
               />
               <input
                 type="text"
                 placeholder="Last Name"
-                className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+                className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-200  focus:outline-none focus:ring-2 focus:ring-green-950"
               />
             </div>
             <input
               type="text"
               placeholder="Company"
-              className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+              className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-200  focus:outline-none focus:ring-2 focus:ring-green-950"
             />
             <input
               type="email"
               placeholder="Email"
-              className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+              className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-200  focus:outline-none focus:ring-2 focus:ring-green-950"
             />
             <input
               type="text"
               placeholder="Phone Number"
-              className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+              className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-200  focus:outline-none focus:ring-2 focus:ring-green-950"
             />
             <textarea
               rows="4"
               placeholder="Message"
-              className="w-full px-3 py-2 rounded-md  placeholder-gray-800  focus:outline-none focus:ring-2 focus:ring-green-950"
+              className="w-full px-3 py-2 rounded-md  placeholder-gray-800 ring-green-800 border-green-800  focus:outline-none focus:ring-2 focus:ring-green-950"
             />
             <div className="flex items-center gap-2">
               <input type="checkbox" className="h-4 w-4 accent-indigo-500" />
