889ca639d6b24392e5576961af05f12187c26f9e
Joan Maina <mainajoan555@gmail.com>
2025-12-23 05:40:07 +0300

    changes in auth user

--- app/src/apis/AuthContext.jsx
+++ app/src/apis/AuthContext.jsx
@@ -1,12 +1,20 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
-const AuthContext = createContext();//creates a global context to check for login an auth status
+const AuthContext = createContext();//creates a global context that can store token,login function,logout  and auth state
 
 export const useAuth = () => useContext(AuthContext); // this reads data from the createcontext
+// that up there is a helper hook that helps us do const { isAuthenticated, login, logout } = useAuth();...instead of writing useContext(AuthContext) every time
 
+
+//now to the brain of auth
+//children are everything wrapped around AuthProvider in main.jsx,in our case its the whole <app />
 export const AuthProvider = ({ children }) => {
   const [accessToken, setAccessToken] = useState(
-    localStorage.getItem("access_token") || null
+    localStorage.getItem("access_token") || null //initially check if token exists in localstorage and helps restore auth state when page is refreshed or reopened,no useeffect needed
+  );
+
+  const [user, setUser] = useState(
+    localStorage.getItem("user") || null //to get name,email..all credentials its important t oadd this
   );
 
   const login = (token) => {
@@ -19,7 +27,11 @@ export const AuthProvider = ({ children }) => {
     setAccessToken(null);
   };
 
-  const isAuthenticated = !!accessToken;
+  const isAuthenticated = !!accessToken; //this is the magic bolean that tells us if user is logged in or not
+  //we wil use this in our add to cart and other protected routes
+  //eg: const { isAuthenticated } = useAuth(); if(!isAuthenticated) navigate("/login");
+  //eg: {isAuthenticated ? <AddToCart /> : <LoginButton />}
+  //eg {isAuthenticated ? <button>logout</button> : <RedirectToLogin />}
 
   return (
     //provides the data 
